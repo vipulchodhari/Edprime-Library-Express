3fb6f04b00233b147f9cee3f2a18187896704bb6
Gaurav Rathore <grathore@ebizneeds.local>
2022-09-27 12:26:23 +0530

    master routes

--- src/components/master/Author.jsx
+++ src/components/master/Author.jsx
@@ -2,10 +2,13 @@ import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow
 import homeIcon from '../../assets/home.png';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import dateFormat from "dateformat";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import '../../styles/master.css';
 
 function formatOurData(columnName, AuthorName, CreationDate, Status) {
-    return { columnName, AuthorName, CreationDate, Status};
+    return { columnName, AuthorName, CreationDate, Status };
 }
 
 const SampleData = [
@@ -16,20 +19,48 @@ const SampleData = [
 ];
 
 export const Author = () => {
+    let [authorData, setAuthorData] = useState();
+    const [query, setQuery] = useState("");
+
+    const getData = async () => {
+        await axios.get('http://192.100.100.52:5000/author')
+            .then((res) => {
+                setAuthorData(res.data.data)
+                console.log(res.data.data)
+            })
+    }
+    // console.log("data", authorData);
+
+    authorData = authorData?.filter((el) =>
+        el.title.toLowerCase().includes(query) ||
+        el.title.toUpperCase().includes(query) ||
+        dateFormat(el.createdAt, "mm-dd-yyyy").toLowerCase().includes(query)
+        // el.status.toLowerCase().includes(query)
+    )
+
+    const searchAuthor = (e) => {
+        setQuery(e.target.value)
+    }
+    console.log("query", query);
+    console.log("filter data", authorData);
+
+    useEffect(() => {
+        getData()
+    }, [])
     return <div className="author-container">
         <h3 className='author-heading'>Set Up</h3>
         <div className='author-top'>
-            <img src={homeIcon} alt=''/>
-            <p style={{fontSize:'12px', color:'#777777'}}>Library, Set Up, Master, Author Master</p>
+            <img src={homeIcon} alt='' />
+            <p style={{ fontSize: '12px', color: '#777777' }}>Library, Set Up, Master, Author Master</p>
         </div>
         <div className="author-cont">
             <div className="author-btnFlex">
-            <h3>Author Master</h3>
-            <button className="author-addbtn">ADD AUTHOR</button>
+                <h3>Author Master</h3>
+                <button className="author-addbtn">ADD AUTHOR</button>
             </div>
-            <hr/>
-            <input className="author-search-box" type='text' placeholder='Search...'/>
-            <p style={{color:'#9EA5B9'}}>Search in all fields</p>
+            <hr />
+            <input onChange={searchAuthor} className="author-search-box" type='text' placeholder='Search...' />
+            <p style={{ color: '#9EA5B9' }}>Search in all fields</p>
             <TableContainer component={Paper}>
                 <Table
                     style={{
@@ -40,7 +71,6 @@ export const Author = () => {
                 >
                     <TableHead>
                         <TableRow>
-                            
                             <TableCell className="book-item-thead">S.No </TableCell>
                             <TableCell align="center" className="book-item-thead">
                                 Author Name
@@ -60,35 +90,34 @@ export const Author = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {SampleData.map((row) => (
-                            <TableRow key={row.columnName}>
+                        {authorData?.map((author, i) => (
+                            <TableRow key={i}>
                                 <TableCell component="th" scope="row" className="book-item-tbody">
-                                    {row.columnName}
+                                    {i + 1}
                                 </TableCell>
                                 <TableCell align="center" className="book-item-tbody">
-                                    <strong>{row.AuthorName}</strong>
+                                    <strong>{author.title}</strong>
                                 </TableCell>
                                 <TableCell align="center" className="book-item-tbody">
-                                    <strong>{row.CreationDate}</strong>
+                                    <strong>{dateFormat(author.createdAt, "mm-dd-yyyy")}</strong>
                                 </TableCell>
                                 <TableCell align="center">
-                                        <div className={
-                                                row.Status==="Approved"?"item-approved"
-                                                :row.Status==="In Progress"?"item-progress"
-                                                :row.Status==="Success"?"item-success"
-                                                :"item-rejected"
-                                            }>
-                                            {row.Status}
-                                        </div>        
+                                    <div className={
+                                        author.status === "Approved" ? "item-approved"
+                                            : author.status === "In Progress" ? "item-progress"
+                                                : author.status === "Success" ? "item-success"
+                                                    : "item-rejected"
+                                    }>
+                                        {author.status === true ? "true" : "false"}
+                                    </div>
                                 </TableCell>
                                 <TableCell align="center" className="book-item-tbody">
-                                    <EditIcon className="author-action-icons"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                    <DeleteIcon className="author-action-icons"/>
+                                    <EditIcon className="author-action-icons" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <DeleteIcon className="author-action-icons" />
                                 </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
-                    
                 </Table>
             </TableContainer>
         </div>
