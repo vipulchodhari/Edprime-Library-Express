ba4de020a6f9df7d25b1659336b717208a5c44e3
vipulchodhari <110665652+vipulchodhari@users.noreply.github.com>
2022-09-29 13:29:07 +0530

    build add master Ui

--- src/components/master/author/AddAuthor.jsx
+++ src/components/master/author/AddAuthor.jsx
@@ -1,42 +1,12 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import homeIcon from '../../../assets/home.png';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import dateFormat from "dateformat";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import '../../../styles/addmaster.css';
-import {Link} from 'react-router-dom'
+import customerBorder from '../../../assets/upload.png';
 
 export const AddAuthor = () => {
-    let [authorData, setAuthorData] = useState();
-    const [query, setQuery] = useState("");
+    const handleUpload = () => {
 
-    const getData = async () => {
-        await axios.get('')
-            .then((res) => {
-                setAuthorData(res.data.data)
-                console.log(res.data.data)
-            })
     }
-    // console.log("data", authorData);
 
-    authorData = authorData?.filter((el) =>
-        el.title.toLowerCase().includes(query) ||
-        el.title.toUpperCase().includes(query) ||
-        dateFormat(el.createdAt, "mm-dd-yyyy").toLowerCase().includes(query)
-        // el.status.toLowerCase().includes(query)
-    )
-
-    const searchAuthor = (e) => {
-        setQuery(e.target.value)
-    }
-    console.log("query", query);
-    console.log("filter data", authorData);
-
-    useEffect(() => {
-         getData()
-    }, [])
     return <div className="author-container">
         <h3 className='author-heading'>Set Up</h3>
         <div className='author-top'>
@@ -47,20 +17,31 @@ export const AddAuthor = () => {
             <div className="author-btnFlex">
                 <h3>Add Author</h3>
                 <div className='display-flex'>
-                <button className="author-addbtn">Submit</button>
-                <button className="author-addbtn">Cancel</button>
+                    <button className="author-addbtn">Submit</button>
+                    <button className="author-addbtn" style={{ backgroundColor: 'rgb(246,78,96)' }}>Cancel</button>
                 </div>
             </div>
             <hr />
-            <div>
-            <p>Author Name</p>
-            <input className="publisher-box" type='text' placeholder='Publisher Name' />
-             
-            </div>
-            <div>
-            <p>Description</p>
-            <input className="publisher-add-box" type='text' />
-            
+            <div className="add-author-container">
+                <div>
+                    <label >Author Name</label><br />
+                    <input className="publisher-box" type='text' placeholder='Publisher Name' /><br />
+                    <label>Description</label><br />
+                    <input className="publisher-box publisher-description" type='text' />
+                </div>
+                <div style={{display: "flex"}}>
+                    <label htmlFor="file-input" className='add-author-img-cont'>
+                        <img src={customerBorder} alt="upload pic" />
+                        {/* <p style={{ marginTop: '0px', color: 'gray' }}><strong>dummy image</strong></p> */}
+                    </label>
+                    <input
+                        style={{ display: 'none', cursor: 'pointer' }}
+                        // style={{ marginTop:'25px' }}
+                        id="file-input"
+                        type='file'
+                        onChange={handleUpload}
+                    />
+                </div>
             </div>
         </div>
     </div>
