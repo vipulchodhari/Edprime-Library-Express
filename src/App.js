e7fe58ded83a1e416e1ffd847a2c1d61eef52303
Gaurav Rathore <grathore@ebizneeds.local>
2022-10-06 10:57:25 +0530

    reservation ui

--- src/App.js
+++ src/App.js
@@ -32,11 +32,8 @@ import { Editlocation } from './components/master/location/Editlocation';
 import { EditSubject } from './components/master/subject/EditSubject';
 import { GeneralSetting } from './components/librarySettings/generalSetting/GeneralSetting';
 import { CheckoutBorrowing } from './components/librarySettings/ceckoutBorrowing/CheckoutBorrowing';
-<<<<<<< HEAD
-import { ReservationsSettings } from './components/librarySettings/reservationsSettings/ReservationSettings';
-=======
 import { Renew } from './components/librarySettings/renew/Renew';
->>>>>>> 13ec07fe612df6573617915337f2b9ff40bd0f74
+import { ReservationsSettings } from './components/librarySettings/reservationsSettings/ReservationSettings';
 
 
 function App() {
@@ -85,11 +82,8 @@ function App() {
 
           <Route path='/general-settings' element={<GeneralSetting/>}></Route>
           <Route path='/checkout-borrowings' element={<CheckoutBorrowing/>}></Route>
-<<<<<<< HEAD
-          <Route path='/reservations-settings' element={<ReservationsSettings/>}></Route>
-=======
           <Route path='/renew' element={<Renew/>}></Route>
->>>>>>> 13ec07fe612df6573617915337f2b9ff40bd0f74
+          <Route path='/reservations' element={<ReservationsSettings/>}></Route>
         </Routes>
       </div>
     </div>
